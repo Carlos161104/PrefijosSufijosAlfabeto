52b2d03d62f3ab67e305f8a0085e45fdc58db476
Carlos161104 <canoja161104@mail.com>
2024-04-30 19:20:09 -0600

    Aplicacion ejemplos tarea

--- build/Index.js
+++ build/Index.js
@@ -2,42 +2,42 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const PefSuf_1 = require("./PefSuf");
 //Primer ejemcicio.
-const cadena1 = "nyab";
+const cadena1 = "aabddcdff";
 const longitud1 = cadena1.split('').length;
 console.log(`Palabra: ${cadena1}`);
 (0, PefSuf_1.Sufijos)(cadena1.split(''), longitud1);
 (0, PefSuf_1.Prefijos)(cadena1.split(''), longitud1);
 console.log("-------------------------------------------------\n\n");
 //Segundo ejercicio.
-const cadena2 = "aabaaca";
+const cadena2 = "bcdcdafaff";
 const longitud2 = cadena2.split('').length;
 console.log(`Palabra: ${cadena2}`);
 (0, PefSuf_1.Sufijos)(cadena2.split(''), longitud2);
 (0, PefSuf_1.Prefijos)(cadena2.split(''), longitud2);
 console.log("-------------------------------------------------\n\n");
 //Tercer ejercicio.
-const cadena3 = "abracadabra";
+const cadena3 = "bcdcdcacaff";
 const longitud3 = cadena3.split('').length;
 console.log(`Palabra: ${cadena3}`);
 (0, PefSuf_1.Sufijos)(cadena3.split(''), longitud3);
 (0, PefSuf_1.Prefijos)(cadena3.split(''), longitud3);
 console.log("-------------------------------------------------\n\n");
 //Cuarto ejercicio.
-const cadena4 = "anitalavalatina";
+const cadena4 = "0110010101";
 const longitud4 = cadena4.split('').length;
 console.log(`Palabra: ${cadena4}`);
 (0, PefSuf_1.Sufijos)(cadena4.split(''), longitud4);
 (0, PefSuf_1.Prefijos)(cadena4.split(''), longitud4);
 console.log("-------------------------------------------------\n\n");
 //Quinto ejercicio.
-const cadena5 = "hola";
+const cadena5 = "010110101100";
 const longitud5 = cadena5.split('').length;
 console.log(`Palabra: ${cadena5}`);
 (0, PefSuf_1.Sufijos)(cadena5.split(''), longitud5);
 (0, PefSuf_1.Prefijos)(cadena5.split(''), longitud5);
 console.log("-------------------------------------------------\n\n");
 //Sexto ejercicio.
-const cadena6 = "hola";
+const cadena6 = "01110001010010";
 const longitud6 = cadena6.split('').length;
 console.log(`Palabra: ${cadena6}`);
 (0, PefSuf_1.Sufijos)(cadena6.split(''), longitud6);
--- src/Index.ts
+++ src/Index.ts
@@ -1,49 +1,49 @@
 import { Sufijos, Prefijos } from "./PefSuf";
 
 //Primer ejemcicio.
-const cadena1: string = "nyab";
+const cadena1: string = "aabddcdff";
 const longitud1: number = cadena1.split('').length;
 console.log(`Palabra: ${cadena1}`);
 Sufijos(cadena1.split(''), longitud1);
 Prefijos(cadena1.split(''), longitud1);
-console.log("-------------------------------------------------\n\n");
+console.log("********************************************************\n\n");
 
 //Segundo ejercicio.
-const cadena2: string = "aabaaca";
+const cadena2: string = "bcdcdafaff";
 const longitud2: number = cadena2.split('').length;
 console.log(`Palabra: ${cadena2}`);
 Sufijos(cadena2.split(''), longitud2);
 Prefijos(cadena2.split(''), longitud2);
-console.log("-------------------------------------------------\n\n");
+console.log("********************************************************\n\n");
 
 //Tercer ejercicio.
-const cadena3: string = "abracadabra";
+const cadena3: string = "bcdcdcacaff";
 const longitud3: number = cadena3.split('').length;
 console.log(`Palabra: ${cadena3}`);
 Sufijos(cadena3.split(''), longitud3);
 Prefijos(cadena3.split(''), longitud3);
-console.log("-------------------------------------------------\n\n");
+console.log("********************************************************\n\n");
 
 //Cuarto ejercicio.
-const cadena4: string = "anitalavalatina";
+const cadena4: string = "0110010101";
 const longitud4: number = cadena4.split('').length;
 console.log(`Palabra: ${cadena4}`);
 Sufijos(cadena4.split(''), longitud4);
 Prefijos(cadena4.split(''), longitud4);
-console.log("-------------------------------------------------\n\n");
+console.log("********************************************************\n\n");
 
 //Quinto ejercicio.
-const cadena5: string = "hola";
+const cadena5: string = "010110101100";
 const longitud5: number = cadena5.split('').length;
 console.log(`Palabra: ${cadena5}`);
 Sufijos(cadena5.split(''), longitud5);
 Prefijos(cadena5.split(''), longitud5);
-console.log("-------------------------------------------------\n\n");
+console.log("********************************************************\n\n");
 
 //Sexto ejercicio.
-const cadena6: string = "hola";
+const cadena6: string = "01110001010010";
 const longitud6: number = cadena6.split('').length;
 console.log(`Palabra: ${cadena6}`);
 Sufijos(cadena6.split(''), longitud6);
 Prefijos(cadena6.split(''), longitud6);
-console.log("-------------------------------------------------\n\n");
+console.log("********************************************************\n\n");
